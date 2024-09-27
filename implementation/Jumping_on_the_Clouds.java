6a3f83048800df6e03dfaffb9a521b59298d0089
Divyanshi-23 <143104948+Divyanshi-23@users.noreply.github.com>
2024-09-27 23:53:03 +0530

    Create Jumping_on_the_Clouds.java

---
+++ implementation/Jumping_on_the_Clouds.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+    // Write your code here
+        int ans = 0;
+        int curr = 0;
+        while(curr!=c.size()-1){
+            if(curr+2<c.size()){
+                if(c.get(curr+2)!=1){
+                    curr = curr+2;
+                    ans++;
+                }else if(c.get(curr+1)==0){
+                    curr = curr+1;
+                    ans++;
+                }
+            }else{
+                curr = curr+1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
